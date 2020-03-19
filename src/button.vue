588a4418139ec303d3ea7e1da13b1aec30480e20
yangyan <15927288912@126.com>
2020-03-19 20:50:33 +0800

    优化 icon 的功能

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,11 @@
 <template>
-  <button class="g-button">
-    <svg v-if='icon' class="icon" aria-hidden="true">
-      <use :xlink:href=`#icon-${icon}` />
-      //<use xlink:href='#icon-settings' />
+  <button class="g-button" :class="{ [`icon-${iconPosition}`]: true }">
+    <svg v-if="icon" class="icon" aria-hidden="true">
+      <use :xlink:href="`#icon-${icon}`" />
     </svg>
-    <slot></slot>
+    <div class='slot'>
+      <slot></slot>
+    </div>
   </button>
 </template>
 
@@ -16,7 +17,28 @@
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
 }
+
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+  margin-right: .3em;
+  order: 1;
+}
+.slot{
+  order:1;
+}
+.icon-right > .icon{
+  order:2;
+}
+
 .g-button:hover {
   border-color: var(--border-color-hover);
 }
@@ -29,6 +51,17 @@
 </style>
 <script>
 export default {
-  props:['icon']
+  // props:['icon']
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: "left",
+      validator(value) {
+        console.log(value)
+        return value === 'left' || value === 'right'
+      }
+    }
+  }
 };
 </script>
